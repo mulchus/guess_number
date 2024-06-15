b303436663de512952e0fe16f062104787e30921
mulchus <mulch@list.ru>
2024-06-15 22:28:33 +0300

    upd to Flask

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
-from fastapi import FastAPI, Request
+from flask import Flask, request, session
+from environs import Env
 
-app = FastAPI()
+env = Env()
+env.read_env()
 
-numbers = {
-    'max_number': 0,
-    'hidden_number': 0,
-    'guess_number': 0,
-}
+app = Flask(__name__)
+app.secret_key = env("SECRET_KEY")
 
 
-@app.get("/set_numbers/")
-def set_numbers(request: Request):
-    global numbers
-    response = request.query_params
-    numbers['max_number'] = int(response['max_number'])
-    numbers['hidden_number'] = int(response['hidden_number'])
-    if numbers['hidden_number'] > numbers['max_number']:
+@app.route('/')
+def start():
+    session.clear()
+    return 'Start ok. First set max_number and hidden_number'
+
+
+@app.route('/set_numbers/')
+def set_numbers():
+    session['max_number'] = int(request.args['max_number'])
+    session['hidden_number'] = int(request.args['hidden_number'])
+    if session['hidden_number'] > session['max_number']:
         return 'hidden_number must be less or equal than max_number'
-    elif numbers['hidden_number'] < 0:
+    elif session['hidden_number'] < 0:
         return 'hidden_number must be greater or equal than 0'
-    return {'numbers': numbers}
+    return {'numbers': session}
 
 
-@app.get("/guess_number/{selected_number}")
+@app.route('/guess_number/<int:selected_number>')
 def guess_number(selected_number):
-    global numbers
-    result = -1 if int(selected_number) > numbers['hidden_number'] else 1 if int(selected_number) < numbers[
+    if 'hidden_number' not in session:
+        return 'First set max_number and hidden_number'
+    result = -1 if selected_number > session['hidden_number'] else 1 if selected_number < session[
         'hidden_number'] else 0
     if not result:
-        numbers['guess_number'] = int(selected_number)
-        return {'result': result, 'guess_number': numbers['guess_number']}
+        session['guess_number'] = selected_number
+        return {'result': result, 'guess_number': session['guess_number']}
     return {'result': result}
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
